56d6e91a2672964ea45097bc4848fc0e12347871
Simone Alessandro Casciaro <95073723+Alit54@users.noreply.github.com>
2021-12-12 23:51:25 +0100

    Add files via upload

--- day06b.go
+++ day06b.go
@@ -15,7 +15,7 @@ func main() {
 		}
 		fish[n]++
 	}
-	for day := 1; day <= 80; day++ {
+	for day := 1; day <= 256; day++ {
 		fish[0], fish[1], fish[2], fish[3], fish[4], fish[5], fish[6], fish[7], fish[8] = fish[1], fish[2], fish[3], fish[4], fish[5], fish[6], fish[0]+fish[7], fish[8], fish[0]
 	}
 	fmt.Println(fish[0] + fish[1] + fish[2] + fish[3] + fish[4] + fish[5] + fish[6] + fish[7] + fish[8])
